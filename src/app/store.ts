47eaaf03ecd8806f12aa681eda63e401d12f39e6
NoScope98 <chulkov_diman@mail.ru>
2021-06-02 01:15:26 +0300

    Добавлен срез для авторизации

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import todosReducer from '../components/todos/todosSlice';
+import authReducer from '../components/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    todos: todosReducer,
+    auth: authReducer,
   },
 });
 
---
+++ src/components/auth/authSlice.ts
@@ -0,0 +1,111 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+import { FetchingStatuses } from '../../utils/constants';
+import { IAuthData, ILoginRequest } from '../../utils/types';
+import { authApi } from './authApi';
+
+const sliceName = 'auth';
+
+export interface IAuthState extends IAuthData {
+  status: FetchingStatuses;
+  isAuthenticated: boolean;
+}
+
+const initialState: IAuthState = {
+  name: '',
+  role: '',
+  status: FetchingStatuses.Idle,
+  isAuthenticated: false,
+};
+
+export const signIn = createAsyncThunk<IAuthData, ILoginRequest>(
+  `${sliceName}/signIn`,
+  async (args, thunkAPI) => {
+    try {
+      const response = await authApi.login(args);
+      console.log('try');
+
+      return response.data;
+    } catch (err) {
+      console.log('err', err);
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const signOut = createAsyncThunk(
+  `${sliceName}/signOut`,
+  async (args, thunkAPI) => {
+    try {
+      const response = await authApi.logout();
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const checkAuthorization = createAsyncThunk<IAuthData, undefined>(
+  `${sliceName}/checkAuthorization`,
+  async (args, thunkAPI) => {
+    try {
+      const response = await authApi.me();
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+const slice = createSlice({
+  name: sliceName,
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(signIn.fulfilled, (state, action) => {
+        state.status = FetchingStatuses.Fulfilled;
+        state.name = action.payload.name;
+        state.role = action.payload.role;
+        state.isAuthenticated = true;
+      })
+      .addCase(signIn.rejected, (state, action) => {
+        state.status = FetchingStatuses.Rejected;
+        state.name = initialState.name;
+        state.role = initialState.role;
+        state.isAuthenticated = false;
+      })
+      .addCase(signIn.pending, (state, action) => {
+        state.status = FetchingStatuses.Pending;
+      })
+
+      .addCase(signOut.fulfilled, (state, action) => {
+        state.status = FetchingStatuses.Fulfilled;
+        state.isAuthenticated = false;
+      })
+      .addCase(signOut.rejected, (state, action) => {
+        state.status = FetchingStatuses.Rejected;
+      })
+      .addCase(signOut.pending, (state, action) => {
+        state.status = FetchingStatuses.Pending;
+      })
+
+      .addCase(checkAuthorization.fulfilled, (state, action) => {
+        state.status = FetchingStatuses.Fulfilled;
+        state.name = action.payload.name;
+        state.role = action.payload.role;
+        state.isAuthenticated = true;
+      })
+      .addCase(checkAuthorization.rejected, (state, action) => {
+        state.status = FetchingStatuses.Rejected;
+        state.name = initialState.name;
+        state.role = initialState.role;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuthorization.pending, (state, action) => {
+        state.status = FetchingStatuses.Pending;
+      });
+  },
+});
+
+export default slice.reducer;
