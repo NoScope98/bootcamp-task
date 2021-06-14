d8a519c4268364728de52887eeabe0305f552732
NoScope98 <chulkov_diman@mail.ru>
2021-06-15 02:51:53 +0300

    Stylize users component
    Add png icons for admin and user.

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { FetchingStatuses } from '../../utils/constants';
+import { FetchingStatuses, Roles } from '../../utils/constants';
 import { handleError } from '../../utils/functionWrappers';
 import { Loader } from '../common/loader/Loader';
 import { fetchAll, userSelectors } from './usersSlice';
+import './users.scss';
+import adminIcon from '../../icons/admin.png';
+import userIcon from '../../icons/user.png';
+
+const iconMapping = {
+  [Roles.Admin]: <img src={adminIcon} alt="adminIcon" />,
+  [Roles.User]: <img src={userIcon} alt="userIcon" />,
+};
 
 export const Users: React.FunctionComponent = () => {
   const dispatch = useAppDispatch();
@@ -18,12 +26,17 @@ export const Users: React.FunctionComponent = () => {
   const isLoading = status === FetchingStatuses.Pending;
 
   return (
-    <ul>
+    <article className="users">
       <Loader show={isLoading} />
       {users.length === 0 && 'There are no available users'}
-      {users.map((user) => (
-        <li key={user.name}>{`${user.name} (${user.role})`}</li>
-      ))}
-    </ul>
+      <ul className="user-list">
+        {users.map((user) => (
+          <li key={user.name} className="user-info">
+            {iconMapping[user.role]}
+            {user.name}
+          </li>
+        ))}
+      </ul>
+    </article>
   );
 };
